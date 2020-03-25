4b43592f6223ce675dd4bd663d803c834992fe91
Mandip Tharu <mandip.chaudhary@yarshastudio.com>
2020-03-25 16:21:39 +0545

    covid 19 dashboard added

---
+++ covid19-stat/precache-manifest.8af402657443212de423ba54efa665f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d680ccfa340f409082b079a675676599",
+    "url": "/covid19-stat/index.html"
+  },
+  {
+    "revision": "3f7454400c815e015ad8",
+    "url": "/covid19-stat/static/css/2.290a3cf6.chunk.css"
+  },
+  {
+    "revision": "c7beb5683cac4f372717",
+    "url": "/covid19-stat/static/css/main.d6d0d428.chunk.css"
+  },
+  {
+    "revision": "3f7454400c815e015ad8",
+    "url": "/covid19-stat/static/js/2.1aac07ce.chunk.js"
+  },
+  {
+    "revision": "f3a464b094a42227540b3ef9a73a0401",
+    "url": "/covid19-stat/static/js/2.1aac07ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7beb5683cac4f372717",
+    "url": "/covid19-stat/static/js/main.8115b11d.chunk.js"
+  },
+  {
+    "revision": "afd33f90222d59d5fb96",
+    "url": "/covid19-stat/static/js/runtime-main.cf7d0fb0.js"
+  }
+]);
